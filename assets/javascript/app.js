1fcadaafe4d0d8c96b56bb0d1c85f0e18b0d4514
Charles Davidson <cddavidson86@yahoo.com>
2019-06-06 22:31:59 -0500

    the prefinalized version without audio. Pre-tweaking of html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,138 +1,211 @@
-var triviaArray = [
+$(document).ready(function() {
+  var triviaArray = [
     {
-        q: "In the arcade game Joust, the player controls a knight riding what flying animal?",
-        a: ["Horse", "Elephant", "Ostrich", "Tiger"],
-        c: 2,
-        image: "assets/images/joust.jpg",
-
+      q:
+        "In the arcade game Joust, the player controls a knight riding what flying animal?",
+      a: ["Horse", "Elephant", "Ostrich", "Tiger"],
+      c: 2,
+      image: "assets/images/joust.jpg"
     },
     {
-        q: "Who said, 'It's time to kick ass, and chew bubble gum....and I'm all out of gum.'",
-        a: ["Duke Nukem", "Kratos", "Master Chief", "Agent 47"],
-        c: 0,
-        image: "<src html='' width= '200px'>",
-    },   
+      q:
+        "Who said, 'It's time to kick ass, and chew bubble gum....and I'm all out of gum.'",
+      a: ["Duke Nukem", "Kratos", "Master Chief", "Agent 47"],
+      c: 0,
+      image: "assets/images/dukenukem.jpg"
+    },
     {
-        q: "What does Geralt of Rivia name every horse he owns?",
-        a: ["Artax", "Sophia", "Rudolf", "Roach"],
-        c: 3,
-        image: "<src html='' width= '200px'>",
+      q: "What does Geralt of Rivia name every horse he owns?",
+      a: ["Artax", "Sophia", "Rudolf", "Roach"],
+      c: 3,
+      image: "assets/images/roach.jpg"
     }
-]
+  ];
 
+  // THE VARIABLES FOR THE GAME
+  var question = 0;
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
+  var unansweredQuestions = 0;
+  var clockRunning = false;
+  var time = 10;
+  var intervalId;
 
-var question = 0;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unansweredQuestions = 0;
+  beginGame();
 
-//STARTING FUNCTIONS
-startTimer();
-populateQuestion();
-populateAnswers();
+  //STARTING FUNCTIONS
+  function beginGame() {
+    question = 0;
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    unansweredQuestions = 0;
+    reset();
+    $("#question-box").html(
+      "<h2>Welcome to the game!</h2>" + "<h2>Press start to play!</h2>"
+    );
+    var startButton = $("<button>");
+    startButton.text("START");
+    startButton.addClass("answer-buttons-animation start");
+    $("#answer-box").append(startButton);
+    // WHEN THE START BUTTON IS CLICKED RUN FUNCTIONS AND CLEAR THE ANSWER BOX
+    $(".start").click(function() {
+      $("#answer-box").empty();
+      startTimer();
+      populateQuestion();
+      populateAnswers();
+    });
+  }
 
-
-// populates the answers in the array of objects
-function populateAnswers() {
+  // POPULATES THE ANSWERS IN THE OBJECT
+  function populateAnswers() {
     for (var i = 0; i < triviaArray[question].a.length; i++) {
-        // building buttons
-        var answerButtons = $("<button>");
-        answerButtons.text(triviaArray[question].a[i]);
-        answerButtons.addClass("when-clicked answer-buttons-animation");
-        answerButtons.attr("id", i);
-        $("#answer-box").append(answerButtons);
+      // CREATING BUTTONS
+      var answerButtons = $("<button>");
+      answerButtons.text(triviaArray[question].a[i]);
+      answerButtons.addClass("when-clicked answer-buttons-animation");
+      answerButtons.attr("value", i);
+      $("#answer-box").append(answerButtons);
     }
-}
+    console.log(triviaArray[question].a);
+  }
 
-// POPULATES THE QUESTION
-function populateQuestion() {
+  // POPULATES THE QUESTION
+  function populateQuestion() {
     $("#question-box").text(triviaArray[question].q);
-}
+    console.log(triviaArray[question].q);
+  }
 
-// IF THE ANSWER IS CORRECT
-function correct() {
+  // IF THE ANSWER IS CORRECT
+  function correct() {
     correctAnswers++;
     $("#question-box").text("You got it right!");
-    $("#answer-box").empty();
-    question++;
-    populateQuestion();
-    populateAnswers();
-}
+    displayImage();
+    console.log("correct " + correctAnswers);
+  }
 
-// IF THE ANSWER IS INCORRECT
-function incorrect() {
+  // IF THE ANSWER IS INCORRECT
+  function incorrect() {
     incorrectAnswers++;
-    $("#question-box").text("Sorry, the correct answer is " + 
-    triviaArray[question].a[triviaArray[question].c] + "!");
-    $("#answer-box").empty();
-    question++;
-    populateQuestion();
-    populateAnswers();
-}
+    $("#question-box").text(
+      "Sorry, the correct answer is " +
+        triviaArray[question].a[triviaArray[question].c] +
+        "!"
+    );
+    displayImage();
+    console.log("incorrect " + incorrectAnswers);
+  }
 
-function unanswered() {
+  // WHEN THE TIME IS UP AND NO ANSWER IS INPUT
+  function unanswered() {
     unansweredQuestions++;
-    $("#question-box").text("Sorry, the correct answer is " + 
-    triviaArray[question].a[triviaArray[question].c] + "!");
+    $("#question-box").text(
+      "Sorry, the correct answer is " +
+        triviaArray[question].a[triviaArray[question].c] +
+        "!"
+    );
+    displayImage();
+    console.log("unanswered " + unansweredQuestions);
+  }
+
+  // DISPLAY THE IMAGE AFTER ANSWER
+  function displayImage() {
+    $("#answer-box").html(
+      "<img src='" +
+        triviaArray[question].image +
+        "' width=200px height=200px />"
+    );
+    $("#answer-box").addClass("pretty");
+    stop();
+    setTimeout(function() {
+      resetImageBox();
+    }, 2000);
+  }
+
+  // RESETS THE IMAGE BOX AND QUESTIONS AND INCREASED QUESTION NUMBER AND REPOPULATES QUESTIONS AND ANSWERS AND CHECKS TO SEE IF THE GAME IS OVER
+  function resetImageBox() {
     $("#answer-box").empty();
-    question++;
-    populateQuestion();
-    populateAnswers();
-}
+    if (question === triviaArray.length - 1) {
+      $("#answer-box").removeClass("pretty");
+      endGame();
+    } else {
+      $("#answer-box").removeClass("pretty");
+      question++;
+      populateQuestion();
+      populateAnswers();
+      console.log(question);
+      reset();
+      startTimer();
+    }
+  }
 
-// RUN AFTER THE LAST QUESTION IS ANSWERED TO END THE GAME
-function endGame() {
-    $("#question-box").html("<h2>FINISH HIM!!</h2>" 
-    + "<h2>Number you got right:</h2>" + correctAnswers
-    + "<h2>Number you got wrong:</h2>" + incorrectAnswers
-    + "<h2>Unanswered:</h2>" + unansweredQuestions);
+  // RUN AFTER THE LAST QUESTION IS ANSWERED TO END THE GAME
+  function endGame() {
+    $("#question-box").html(
+      "<h2>FINISH HIM!!</h2>" +
+        "<h2>Number you got right:</h2>" +
+        correctAnswers +
+        "<h2>Number you got wrong:</h2>" +
+        incorrectAnswers +
+        "<h2>Unanswered:</h2>" +
+        unansweredQuestions
+    );
+    var restartButton = $("<button>");
+    restartButton.text("RESTART");
+    restartButton.addClass("answer-buttons-animation restart");
+    $("#answer-box").append(restartButton);
+    // WHEN THE RESTART BUTTON IS CLICKED RUN FUNCTIONS AND CLEAR THE ANSWER BOX
+    $(".restart").click(function() {
+      $("#answer-box").empty();
+      beginGame();
+    });
+    console.log("Game is over.");
+  }
 
-}
+  // STARTING ON TIMING FUNCTIONS AND VARIABLES AFTER THIS LINE
 
-// STARTING ON TIMING FUNCTIONS AFTER THIS LINE
-
-
-var time = 10;
-var intervalId;
-
-// RESETS THE TIME TO 30 SECONDS
-function reset() {
+  // RESETS THE TIME TO 10 SECONDS
+  function reset() {
     time = 10;
-    // DONE: Change the "display" div to "00:00."
     $("#timer-box").text("00:00");
   }
 
-// BEGINS THE TIMER
+  // BEGINS THE TIMER
   function startTimer() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-
-// DECREMENT FUNCTIONS
-  function decrement() {
-    time--;
-
-    $("#timer-box").html("<p>00:" + time + "</p>");
-
-    if (time === 0) {
-      reset();
-      incorrect();
+    if (!clockRunning) {
+      intervalId = setInterval(decrement, 1000);
+      clockRunning = true;
     }
   }
 
-// STARTING ON CLICK FUNCTIONS AFTER THIS LINE
+  // STOP THE CLOCK
+  function stop() {
+    clearInterval(intervalId);
+    clockRunning = false;
+    console.log("stop time");
+  }
 
-$(document).ready(function(){
+  // DECREMENT FUNCTIONS
+  function decrement() {
+    time--;
+    $("#timer-box").html("<p>00:" + time + "</p>");
+    if (time === 0) {
+      reset();
+      unanswered();
+    }
+  }
 
-  $(".when-clicked").click(function(){
+  // ON CLICK METHODS AND TESTING IF ANSWERS ARE CORRECT OR NOT
+  $(document).on("click", ".when-clicked", function(event) {
+    console.log(parseInt("the value of this " + this.value));
+    console.log(
+      parseInt("the value of the array c " + triviaArray[question].c)
+    );
+    if (parseInt(this.value) === parseInt(triviaArray[question].c)) {
+      correct();
+    } else {
+      incorrect();
+    }
 
-    alert($(this).attr("id"));
-    console.log($(this).attr("id"));
-
+    console.log(parseInt(this.value));
   });
-
-function displayImage () {
-    $("#answer-box").html("<img src='" + triviaArray[question].image + "' width=200px height=200px align=right />");
-}
-
 });
